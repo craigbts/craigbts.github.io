1c2230dcc4d66484b87544a05ef02736c438d5f5
Craig <craig.beatts@gmail.com>
2018-06-25 20:11:48 -0700

    Personal Website

---
+++ scripts/scripts.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("Ready!");
+});
+
+var x = document.getElementById("navLinks");
+var y = document.getElementById('btn');
+x.style.display = "none";
+var z = window.matchMedia("(min-width: 1170px)")
+
+function query(z) {
+  if (z.matches) {
+    x.style.display = "inline-block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+query(z);
+z.addListener(query)
+
+
+function myFunction() {
+  if (z.matches == false) {
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      y.innerHTML = '<i class="material-icons"  id="navBtn">expand_less</i>'
+    } else {
+      x.style.display = "none";
+      y.innerHTML = '<i class="material-icons" id="navBtn">expand_more</i>'
+    }
+  }
+};
